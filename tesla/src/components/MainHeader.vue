70236f7030400e97485ece8d17559c207125a9ef
Maria Eugenia Costa <costamariaeugenia1@gmail.com>
2024-03-08 18:13:00 -0300

    feat: hero section

--- tesla/src/components/MainHeader.vue
+++ tesla/src/components/MainHeader.vue
@@ -1,3 +1,8 @@
 <template>
-  <div>This is my MainHeader component</div>
+  <div class="main-header container mx-auto bg-cover md:bg-contain bg-no-repeat bg-left-top md:bg-center relative -top-16" :style="`background-image: url(${Hero}); height: 100vh;`">
+  </div>
 </template>
+
+<script setup>
+import Hero from "../assets/tesla-hero.png";
+</script>
--- tesla/src/components/Nav.vue
+++ tesla/src/components/Nav.vue
@@ -1,41 +1,36 @@
 <template>
-  <div class="nav container mx-auto my-8 grid grid-flow-col grid-cols-2 gap-4">
-    <img :src="Logo" alt="Tesla logo in black and white" class="w-16" />
-    <div class="hidden md:block">
-      <div class="grid grid-flow-col grid-cols-4 gap-4 items-center">
-      <p
-        v-for="(option, optionIndex) in menu"
-        :key="optionIndex"
-        @click="openLink(option.link)"
-        class="cursor-pointer text-sm font-bold"
-      >
-        {{ option.title }}
-      </p>
+  <div class="nav container mx-auto my-8 grid grid-flow-col grid-cols-1 md:grid-cols-4 gap-4">
+    <div>
+      <img :src="Logo" width="56" height="56" alt="Tesla logo in black and white" class="w-14 mx-auto md:mx-0" />
     </div>
+    <div class="hidden md:grid col-span-3">
+      <div class="grid grid-flow-col grid-cols-5 gap-4 items-center h-full">
+        <p
+          v-for="(option, optionIndex) in menu"
+          :key="optionIndex"
+          @click="openLink(option.link)"
+          class="cursor-pointer text-sm font-bold"
+        >
+          {{ option.title }}
+        </p>
+      </div>
+    </div>
+    <div class="grid col-span-3 md:hidden fixed right-8 top-8 cursor-pointer">
+      <img :src="Menu" width="56" height="56" alt="Menu icon in black and white" class="w-8" />
     </div>
   </div>
 </template>
 
 <script setup>
 import Logo from "../assets/tesla-icon.png";
+import Menu from "../assets/menu.png";
 
 const menu = [
-  {
-    title: "Brand",
-    link: "",
-  },
-  {
-    title: "Brand 2",
-    link: "",
-  },
-  {
-    title: "Brand 3",
-    link: "",
-  },
-  {
-    title: "Brand 4",
-    link: "",
-  },
+  { title: "Model S", link: "" },
+  { title: "Model 3", link: "" },
+  { title: "Model X", link: "" },
+  { title: "Model Y",  link: "" },
+  { title: "cybertruck",  link: "" }
 ];
 
 const openLink = (link) => {
