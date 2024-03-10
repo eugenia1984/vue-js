d24bc4394cca75d33ce659915b0b62dcb45202f4
Maria Eugenia Costa <costamariaeugenia1@gmail.com>
2024-03-10 17:53:52 -0300

    feat: stats

--- tesla/src/components/Stats.vue
+++ tesla/src/components/Stats.vue
@@ -1,8 +1,7 @@
 <template>
-  <div class="stats bg-cover bg-no-repeat bg-bottom" :style="`background-image: url(${TeslaBackground}); height: 450px;`">
-    <div class="w-full h-full bg-blue-950 opacity-75 absolute z-0" />
-    <div class="stats-components z-50 relative container mx-auto">
-      <p class="text-white">{{ cards }}</p>
+  <div class="stats bg-cover bg-no-repeat bg-bottom relative pt-24 mb-48" :style="`background-image: url(${TeslaBackground}); height: 332px; background-position: center bottom-6rem;`">
+    <div class="w-full h-full bg-blue-950 opacity-75 absolute top-0 left-0 z-0" />
+    <div class="stats-components z-50 relative container mx-auto flex items-center justify-between">
       <StatsCard v-for="obj, objIndex in cards" :key="objIndex" :content="obj"/>
     </div>
   </div>
--- tesla/src/components/elements/stats/StatsCard.vue
+++ tesla/src/components/elements/stats/StatsCard.vue
@@ -1,7 +1,8 @@
 <template>
-  <section class="stats-card text-black text-center">
-    <h2 class="text-xl font-bold mb-2">{{ content.title }}</h2>
-    <p class="font-light mb-4">{{ content.description }} </p>
+  <section class="stats-card flex flex-col items-center justify-start text-black text-center bg-white p-16 shadow-lg">
+    <img class="h-16 mb-8" :src="content.imgSrc" :alt="content.title">
+    <h2 class="text-xl font-light mb-2">{{ content.title }}</h2>
+    <p class="font-bold text-sm" v-html="content.description" />
   </section>
 </template>
 
@@ -12,3 +13,12 @@ export default {
   mixins: [GeneralMixin]
 }
 </script>
+
+<style>
+  .stats-card {
+    min-width: 240px;
+    max-width: 240px;
+    min-height: 300px;
+    max-height: 300px;
+  }
+</style>
